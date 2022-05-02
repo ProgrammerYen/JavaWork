89b3f48fea1688cf7ed58cf604ae100ac25ea582
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2022-05-02 10:54:25 +0100

    Added first java program.

---
+++ scripts/first_java_program.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world. I am new to java, but have experience in Python. Let's begin this journey.")
+    }
+}
